ab74bec116e8029cf322cd87036176f3039590b1
Talissa <liayai@ya.ru>
2024-06-20 13:54:14 +0300

    Add alt for img and take out const

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+// @todo: DOM узлы
+const cardTemplateItem = cardTemplate.querySelector('.card');
 const cardList = document.querySelector('.places__list');
 
+
+// @todo: Функция создания карточки
 function addCard (card, delCard) {
-  // @todo: Темплейт карточки
-  const cardTemplate = document.querySelector('#card-template').content;
-
-  // @todo: DOM узлы
-  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-
-  // @todo: Функция создания карточки
+  const cardItem = cardTemplateItem.cloneNode(true);
+  
   cardItem.querySelector('.card__image').src = card.link;
+  cardItem.querySelector('.card__image').alt = card.name;
   cardItem.querySelector('.card__title').textContent = card.name;
 
   cardItem.querySelector('.card__delete-button').addEventListener('click', () => delCard(cardItem));
